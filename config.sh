ebd3be57d8f77af9e4c88eab218f28b0f40f4056
José David Rico Días <davidricodias.dev@gmail.com>
2022-10-18 22:23:11 +0200

    Deleted clone.sh due to avignon incompatibilities. Improved config

--- config.sh
+++ config.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
+# Clean
+rm -rf image-aos
+rm -rf image-soa
+
 # Build directories
 mkdir -p image-aos
 mkdir -p image-soa
 
-if [[ $# == 1 && $1 == "avignon" ]]; then
+if [ $# -eq 1 ] && [ $1 == "avignon" ]; then
 # Config build directories for Avignon
 	cmake -S . -B image-aos -DCMAKE_CXX_COMPILER=/opt/gcc-12.1.0/bin/c++ -DCMAKE_BUILD_TYPE=Release
 	cmake -S . -B image-soa -DCMAKE_CXX_COMPILER=/opt/gcc-12.1.0/bin/c++ -DCMAKE_BUILD_TYPE=Release
-elif [[ $# == 1 && $1 == "macos" ]]; then
+elif [ $# -eq 1 ] && [ $1 == "macos" ]; then
 # Config build directories for MacOS
 	cmake -S . -B image-aos -DCMAKE_CXX_COMPILER=/opt/homebrew/bin/g++-12 -DCMAKE_BUILD_TYPE=Release
 	cmake -S . -B image-soa -DCMAKE_CXX_COMPILER=/opt/homebrew/bin/g++-12 -DCMAKE_BUILD_TYPE=Release
@@ -18,5 +22,5 @@ else
 fi
 
 # Build targets on independent directories
-cmake --build image-soa --target imgsoa
-cmake --build image-aos --target imgaos
+cmake --build image-soa --target imgsoa --clean-first
+cmake --build image-aos --target imgaos --clean-first
