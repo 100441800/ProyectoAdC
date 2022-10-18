d19ac2ce23d72d8afa5c51ea2ee2c52f9e9ee3bd
José David Rico Días <davidricodias.dev@gmail.com>
2022-10-18 22:52:27 +0200

    Adapt config to avignon. Add 2 tests

--- config.sh
+++ config.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+#SBATCH -o /config.out
+#SBATCH -e /config.out
 
 # Clean
 rm -rf image-aos
@@ -8,11 +10,11 @@ rm -rf image-soa
 mkdir -p image-aos
 mkdir -p image-soa
 
-if [ $# -eq 1 ] && [ $1 == "avignon" ]; then
+if [ $# -eq 1 ] && [ $1 == avignon ]; then
 # Config build directories for Avignon
 	cmake -S . -B image-aos -DCMAKE_CXX_COMPILER=/opt/gcc-12.1.0/bin/c++ -DCMAKE_BUILD_TYPE=Release
 	cmake -S . -B image-soa -DCMAKE_CXX_COMPILER=/opt/gcc-12.1.0/bin/c++ -DCMAKE_BUILD_TYPE=Release
-elif [ $# -eq 1 ] && [ $1 == "macos" ]; then
+elif [ $# -eq 1 ] && [ $1 == macos ]; then
 # Config build directories for MacOS
 	cmake -S . -B image-aos -DCMAKE_CXX_COMPILER=/opt/homebrew/bin/g++-12 -DCMAKE_BUILD_TYPE=Release
 	cmake -S . -B image-soa -DCMAKE_CXX_COMPILER=/opt/homebrew/bin/g++-12 -DCMAKE_BUILD_TYPE=Release
@@ -24,3 +26,5 @@ fi
 # Build targets on independent directories
 cmake --build image-soa --target imgsoa --clean-first
 cmake --build image-aos --target imgaos --clean-first
+
+tail -f ./config.out
---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./image-soa/imgsoa a b 
+./image-aos/imgaos a b 
