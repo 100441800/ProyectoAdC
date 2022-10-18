d6fd0124fdd95bc09f0da3936cca438381d52fe7
José David Rico Días <davidricodias.dev@gmail.com>
2022-10-18 22:55:01 +0200

    Update CMakeLists and config for avignon structure

--- config.sh
+++ config.sh
@@ -12,8 +12,8 @@ mkdir -p image-soa
 
 if [ $# -eq 1 ] && [ $1 == avignon ]; then
 # Config build directories for Avignon
-	cmake -S . -B image-aos -DCMAKE_CXX_COMPILER=/opt/gcc-12.1.0/bin/c++ -DCMAKE_BUILD_TYPE=Release
-	cmake -S . -B image-soa -DCMAKE_CXX_COMPILER=/opt/gcc-12.1.0/bin/c++ -DCMAKE_BUILD_TYPE=Release
+	cmake -S . -B image-aos -DCMAKE_CXX_COMPILER=/opt/gcc-12.1.0/bin/g++ -DCMAKE_BUILD_TYPE=Release
+	cmake -S . -B image-soa -DCMAKE_CXX_COMPILER=/opt/gcc-12.1.0/bin/g++ -DCMAKE_BUILD_TYPE=Release
 elif [ $# -eq 1 ] && [ $1 == macos ]; then
 # Config build directories for MacOS
 	cmake -S . -B image-aos -DCMAKE_CXX_COMPILER=/opt/homebrew/bin/g++-12 -DCMAKE_BUILD_TYPE=Release
