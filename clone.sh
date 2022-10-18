0511cfd3450eb654f258b526c852eaf2ec8eefac
José David Rico Días <davidricodias.dev@gmail.com>
2022-10-18 22:04:42 +0200

    Add avignon and macos support. Add cloning from remote repo

---
+++ clone.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+git clone https://github.com/100441800/ProyectoAdC.git .
--- config.sh
+++ config.sh
@@ -4,9 +4,18 @@
 mkdir -p image-aos
 mkdir -p image-soa
 
-# Config build directories
-cmake -S . -B image-aos -DCMAKE_C_COMPILER=/opt/homebrew/bin/gcc-12 -DCMAKE_CXX_COMPILER=/opt/homebrew/bin/g++-12 -DCMAKE_BUILD_TYPE=Release
-cmake -S . -B image-soa -DCMAKE_C_COMPILER=/opt/homebrew/bin/gcc-12 -DCMAKE_CXX_COMPILER=/opt/homebrew/bin/g++-12 -DCMAKE_BUILD_TYPE=Release
+if [[ $# == 1 && $1 == "avignon" ]]; then
+# Config build directories for Avignon
+	cmake -S . -B image-aos -DCMAKE_CXX_COMPILER=/opt/gcc-12.1.0/bin/c++ -DCMAKE_BUILD_TYPE=Release
+	cmake -S . -B image-soa -DCMAKE_CXX_COMPILER=/opt/gcc-12.1.0/bin/c++ -DCMAKE_BUILD_TYPE=Release
+elif [[ $# == 1 && $1 == "macos" ]]; then
+# Config build directories for MacOS
+	cmake -S . -B image-aos -DCMAKE_CXX_COMPILER=/opt/homebrew/bin/g++-12 -DCMAKE_BUILD_TYPE=Release
+	cmake -S . -B image-soa -DCMAKE_CXX_COMPILER=/opt/homebrew/bin/g++-12 -DCMAKE_BUILD_TYPE=Release
+else
+	echo "Incorrect arguments: use macos or avignon"
+	exit -1
+fi
 
 # Build targets on independent directories
 cmake --build image-soa --target imgsoa
